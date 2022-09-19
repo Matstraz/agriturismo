dbe195d2a12f634ec9487d24e73b7918c4e357a5
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2022-09-19 11:06:40 +0200

    feat: add i18n

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useTranslation } from "react-i18next";
+import i18n from "./utilities/i18n";
+
 function App() {
-  return <div></div>;
+  const { t } = useTranslation();
+
+  function handleLanguage(event) {
+    i18n.changeLanguage(event.target.value);
+  }
+
+  return (
+    <div>
+      <div>{t("title")}</div>;
+      <button onClick={handleLanguage} value={"en"}>
+        english
+      </button>
+      <button onClick={handleLanguage} value={"it"}>
+        italian
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "../src/utilities/i18n.js";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
